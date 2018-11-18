90a0ca39a1ef414b43fe0f81c34c6bcf8314a908
diegogue <operacioncomadreja@gmail.com>
2018-11-18 03:05:51 -0600

    Distribución normal y clase DispositivoIO

--- src/estructura/componentes/DispositivoIO.java
+++ src/estructura/componentes/DispositivoIO.java
@@ -4,6 +4,10 @@ import estructura.simulacion.Distribuciones;
 import estructura.simulacion.Programa;
 import estructura.Simulacion;
 
+import java.util.Random;
+
+import static java.lang.Math.sqrt;
+
 public class DispositivoIO extends Modulo {
 
     private final int media;
@@ -13,11 +17,29 @@ public class DispositivoIO extends Modulo {
         super(simulacion, siguienteModulo, numeroServidores);
 
         media = 1000; // 1 seg -> 1000 milisegundos
-        desviacionEstandar = Math.sqrt(10000); // 0.01 segundos^2 -> 10000 milsegundos^2
+        desviacionEstandar = sqrt(10000); // 0.01 segundos^2 -> 10000 milsegundos^2
+    }
+
+    /*
+     * Tiempo de salida entre 40s y 20 s
+     * */
+    @Override
+    protected double getTiempoSalida(Programa programa) {
+        Random rand = new Random();
+        return sqrt((rand.nextDouble() * 3) + 1);
     }
 
     @Override
-    protected double getTiempoSalida(Programa programa) {
-        return Distribuciones.generarValorDistribucionNormal(media, desviacionEstandar);
+    public void procesarSalida(Programa programa) {
+
+        estadisticasComponente.anadirTiempoServicio(
+                programa.getEstadisticaPrograma().getTiempoDeVida(simulacion.getReloj()));
+
+        simulacion.getEstadisticas().anadirNumeroConexionesCompletadas();
+        simulacion.getEstadisticas().anadirTiempoConsultaFinalizada(
+                programa.getEstadisticaPrograma().getTiempoDeVida(simulacion.getReloj()));
+        /**/
     }
+
+
 }
